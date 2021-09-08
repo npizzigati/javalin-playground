cc5c1cb2db229b12b63b03d52b9ede0c807257c7
Nick Pizzigati <npizzigati@gmail.com>
2021-09-07 19:50:43 -0500

    Fix port assignment for Heroku

--- src/main/java/com/nativeword/playground/App.java
+++ src/main/java/com/nativeword/playground/App.java
@@ -3,9 +3,18 @@ package com.nativeword.playground;
 import io.javalin.Javalin;
 
 public class App {
-    public static void main(String[] args) {
-        Javalin app = Javalin.create().start(7000);
-        app.get("/", ctx -> ctx.result("Hello World"));
-        app.get("/toolong", ctx -> ctx.result("Gawd this took way too long."));
+  public static void main(String[] args) {
+      Javalin app = Javalin.create()
+        .start(getHerokuAssignedPort())
+      app.get("/", ctx -> ctx.result("Hello World"));
+      app.get("/toolong", ctx -> ctx.result("Gawd this took way too long."));
+  }
+
+  private static int getHerokuAssignedPort() {
+    String herokuPort = System.getenv("PORT");
+    if (herokuPort != null) {
+      return Integer.parseInt(herokuPort);
     }
+    return 7000;
+  }
 }
